f435d428c0467fc0de4e228000580b2851538057
Simone Lazzaris <slazzaris@gmail.com>
2023-08-17 17:40:43 +0200

    Added copyright message

--- k8s-audit-webhook/collections.go
+++ k8s-audit-webhook/collections.go
@@ -1,3 +1,18 @@
+/*
+Copyright 2023 Codenotary Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
 import (
@@ -32,15 +47,20 @@ func createCollection() {
 			{"stage", "STRING"},
 			{"level", "STRING"},
 			{"requestReceivedTimestamp", "STRING"},
-			{"stageTimestamp", "STRING"},
 			{"userAgent", "STRING"},
 			{"verb", "STRING"},
+			{"objectRef.namespace", "STRING"},
+			{"objectRef.resource", "STRING"},
+			{"objectRef.name", "STRING"},
 		},
 		Indexes: []collIndex{
 			// {Fields: []string{"auditID", "stage"}, IsUnique: true},
 			{Fields: []string{"auditID"}, IsUnique: false},
 			{Fields: []string{"stage"}, IsUnique: false},
 			{Fields: []string{"level"}, IsUnique: false},
+			{Fields: []string{"objectRef.namespace"}, IsUnique: false},
+			{Fields: []string{"objectRef.resource"}, IsUnique: false},
+			{Fields: []string{"objectRef.name"}, IsUnique: false},
 		},
 	}
 	url := fmt.Sprintf("%s/ics/api/v1/ledger/%s/collection/%s", config.url, config.ledger, config.collection)
